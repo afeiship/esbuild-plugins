eee9c2a6e17c827161c03e8199f948c09f1d5209
afeiship <1290657123@qq.com>
2025-04-25 19:59:03 +0800

    feat: init sync verison plugin

---
+++ packages/esbuild-plugin-sync-version/README.md
@@ -0,0 +1,36 @@
+# esbuild-plugin-sync-version
+> Esbuild sync verion plugin.
+
+[![version][version-image]][version-url]
+[![license][license-image]][license-url]
+[![size][size-image]][size-url]
+[![download][download-image]][download-url]
+
+## installation
+```shell
+yarn add @jswork/esbuild-plugin-sync-version
+```
+
+## usage
+```js
+import tsupLibrary from '@jswork/esbuild-plugin-sync-version';
+
+tsupLibrary(1024);
+
+// [1000, 0, 20, 4]
+```
+
+## license
+Code released under [the MIT license](https://github.com/afeiship/esbuild-plugin-sync-version/blob/master/LICENSE.txt).
+
+[version-image]: https://img.shields.io/npm/v/@jswork/esbuild-plugin-sync-version
+[version-url]: https://npmjs.org/package/@jswork/esbuild-plugin-sync-version
+
+[license-image]: https://img.shields.io/npm/l/@jswork/esbuild-plugin-sync-version
+[license-url]: https://github.com/afeiship/esbuild-plugin-sync-version/blob/master/LICENSE.txt
+
+[size-image]: https://img.shields.io/bundlephobia/minzip/@jswork/esbuild-plugin-sync-version
+[size-url]: https://github.com/afeiship/esbuild-plugin-sync-version/blob/master/dist/esbuild-plugin-sync-version.min.js
+
+[download-image]: https://img.shields.io/npm/dm/@jswork/esbuild-plugin-sync-version
+[download-url]: https://www.npmjs.com/package/@jswork/esbuild-plugin-sync-version
---
+++ packages/esbuild-plugin-sync-version/src/index.ts
@@ -0,0 +1,51 @@
+import { Plugin } from 'esbuild';
+import fs from 'fs';
+import path from 'path';
+
+// 定义插件选项的类型
+interface PluginOptions {
+  matchContent?: RegExp; // 匹配内容的正则表达式，默认匹配 __VERSION__
+}
+
+// 插件主函数
+function createEsbuildPlugin(options: PluginOptions = {}): Plugin {
+  const { matchContent = /__VERSION__/g } = options;
+
+  return {
+    name: 'custom-esbuild-plugin', // 插件名称
+    setup(build) {
+      // 获取 package.json 的路径
+      const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+
+      // 读取 package.json 并提取 version 字段
+      let version: string;
+      try {
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+        if (!packageJson.version) {
+          throw new Error('package.json 中未找到 version 字段');
+        }
+        version = packageJson.version;
+      } catch (err) {
+        console.error('读取 package.json 失败:', err.message);
+        process.exit(1);
+      }
+
+      // 监听文件加载事件
+      build.onLoad({ filter: /.*/ }, async (args) => {
+        // 读取文件内容
+        const contents = await fs.promises.readFile(args.path, 'utf-8');
+
+        // 替换匹配的内容为 version
+        const replacedContents = contents.replace(matchContent, version);
+
+        // 返回处理后的内容
+        return {
+          contents: replacedContents,
+          loader: 'default', // 保持默认加载器
+        };
+      });
+    },
+  };
+}
+
+export default createEsbuildPlugin;
